f453fbef11cd7eac7bf03072b2224fa4bfd40f8b
Revuri <manirevu@147dda8c7f36.ant.amazon.com>
2021-07-19 11:38:50 +0530

    changed build script-7

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
 import { Component } from '@angular/core';
+import { FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
+@Component({
+  selector: 'app-reactive-favorite-color',
+  template: `
+    Favorite Color: <input type="text" [formControl]="favoriteColorControl">
+  `
+})
+
+
 export class AppComponent {
   title = 'test-app';
+  favoriteColorControl = new FormControl('');
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { Component } from '@angular/core';
-import { FormControl } from '@angular/forms';
+
 
 import { AppComponent } from './app.component';
 
-@Component({
-  selector: 'app-reactive-favorite-color',
-  template: `
-    Favorite Color: <input type="text" [formControl]="favoriteColorControl">
-  `
-})
 
-export class FavoriteColorComponent {
-  favoriteColorControl = new FormControl('');
-}
+
 
 @NgModule({
   declarations: [
